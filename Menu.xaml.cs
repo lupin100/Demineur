fda4df30c0e72cc2dbc2b2e9287b501ba069343e
Shuuss <tanguyabdoulvaid@gmail.com>
2024-01-12 19:40:41 +0100

    feat : implémentations de clément

--- Menu.xaml.cs
+++ Menu.xaml.cs
@@ -23,5 +23,37 @@ namespace Démineur
         {
             InitializeComponent();
         }
+        
+        private void annuler_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        private void jouer_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        public string difficulte;
+
+        private void facile_Click(object sender, RoutedEventArgs e)
+        {
+            difficulte = "facile";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            difficulte = "moyen";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            difficulte = "difficile";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            difficulte = "personnalisé";
+        }
     }
 }
